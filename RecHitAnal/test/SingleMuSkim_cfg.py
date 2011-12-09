234e9f0a0f974aea000a5c8e3b274d27867a2637
mskim <>
2011-12-09 00:25:32 +0000

    update SingleMuSkim_cfg

--- RecHitAnal/test/SingleMuSkim_cfg.py
+++ RecHitAnal/test/SingleMuSkim_cfg.py
@@ -1,31 +1,51 @@
 import FWCore.ParameterSet.Config as cms
-process = cms.Process("Demo")
+import os
+
+process = cms.Process("Skim")
 
 # initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = "START42_V12::All"
-process.prefer("GlobalTag")
-
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = 'START42_V12::All' #Summer11 MC
+process.GlobalTag.globaltag = 'GR_R_42_V14::All' #2011A data
+
+##from Configuration.PyReleaseValidation.autoCond import autoCond
+##--This file is getting obsolete, please use Configuration.AlCa.autoCond instead
+#from Configuration.AlCa.autoCond import autoCond
+#process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
+
+## Source
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+#process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring(
+#    pickRelValInputFiles( cmsswVersion  = os.environ['CMSSW_VERSION']
+#                        , relVal        = 'RelValTTbar'
+#                        , globalTag     = process.GlobalTag.globaltag.value().split(':')[0]
+#                        , numberOfFiles = 1
+#                        )
+#    )
+#)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-    #'/../user/h/hkseo/RPC/ZMuSkim/zmumuJet_384_10_1_Ibq.root'
-    'file:/tmp/hkseo/FE67038D-A57C-E011-9E83-0030487F16BF.root'
+    #'file:/korserv001/hkseo/data/Run2011A/SingleMu-ZMuSkim/Run2011A-ZMuSkim_55_1_0IG.root'
+    'rfio:/castor/cern.ch/user/h/hkseo/data/Run2011A/ZMuSkim/Run2011A-ZMuSkim_55_1_0IG.root'
     )
 )
 
 # TagMu Skim
-process.load("UserCode.SKKU.TagMuSkim_cff")
+#process.load("SKKU.RecHitAnal.TagMuSkim_cff")
+process.load("TagMuSkim_cff")
 
 # Good vertex requirement
 process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
@@ -36,13 +56,13 @@ process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
                                            )
 
 
-process.p1 = cms.Path(process.TagMuSelSeq*process.primaryVertexFilter)
+process.p1 = cms.Path(process.primaryVertexFilter*process.TagMuSelSeq)
 #process.p1 = cms.Path(process.diMuonSelSeq*process.demo)
 #process.p2 = cms.Path(process.pfMetWMuNuSeq*process.demo)
 
 # Output module configuration
 process.out = cms.OutputModule("PoolOutputModule",
-                               fileName = cms.untracked.string('ZMuSkim-Summer11.root'),
+                               fileName = cms.untracked.string('Skim.root'),
                                # save only events passing the full path
                                SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p1') )
                                )
