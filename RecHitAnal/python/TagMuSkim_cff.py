612c4d982b0e187b43f047acb53c8d6c14d61851
mskim <>
2012-01-05 15:16:58 +0000

    included Z filter

--- RecHitAnal/python/TagMuSkim_cff.py
+++ RecHitAnal/python/TagMuSkim_cff.py
@@ -14,7 +14,7 @@ ZMuHLTFilter.HLTPaths = ["HLT_Mu*","HLT_IsoMu*","HLT_DoubleMu*"]
 looseMuonsForZ = cms.EDFilter("MuonSelector",
                              src = cms.InputTag("muons"),
                              cut = cms.string('pt > 10 && abs(eta)<2.4 && isGlobalMuon = 1 && isTrackerMuon = 1 && abs(innerTrack().dxy)<2.0'),
-                             filter = cms.bool(True)                                
+                             filter = cms.bool(True)
                              )
 
 tightMuonsForZ = cms.EDFilter("MuonSelector",
@@ -26,14 +26,48 @@ tightMuonsForZ = cms.EDFilter("MuonSelector",
 generalTracksFilter = cms.EDFilter("TrackCountFilter",
                                    src = cms.InputTag('generalTracks'),
                                    cut = cms.string('pt > 5 && abs(eta)<2.4'),
-                                   minNumber = cms.uint32(2)
+                                   minNumber = cms.uint32(2) 
                                    )
+
+goodTracks = cms.EDFilter("TrackSelector",
+                          src = cms.InputTag("generalTracks"), # or cms.InputTag("standAloneMuons","UpdatedAtVtx"),
+                          #cut = cms.string("numberOfValidHits >= 10 && normalizedChi2 < 5 && abs(d0) < 2 && abs(dz) < 30"),
+                          cut = cms.string("numberOfValidHits >= 10"),
+                          )
+
+trackCands  = cms.EDProducer("ConcreteChargedCandidateProducer",
+                             src  = cms.InputTag("goodTracks"),
+                             particleType = cms.string("mu+"),     # this is needed to define a mass
+                             )
+
+trackProbes = cms.EDFilter("CandViewRefSelector",
+                           src = cms.InputTag("trackCands"),
+                           #cut = cms.string("pt>15 && abs(eta)<1.8"),
+                           cut = cms.string("pt>5 && abs(eta)<1.8"),
+                           )
+
+# build Z-> MuMu candidates
+dimuons = cms.EDProducer("CandViewShallowCloneCombiner",
+                         checkCharge = cms.bool(False),
+                         cut = cms.string('mass > 30'),
+                         decay = cms.string("tightMuonsForZ@+ trackProbes@-") # charge conjugate states are implied; 'tagMuons' and 'trkProbes' should be collections of Candidates
+                         )
+
+# Z filter
+dimuonsFilter = cms.EDFilter("CandViewCountFilter",
+                             src = cms.InputTag("dimuons"),
+                             minNumber = cms.uint32(1)
+                             )
 
 # TagMu Skim sequence
 TagMuSelSeq = cms.Sequence(ZMuHLTFilter *
                            looseMuonsForZ *
-                           tightMuonsForZ *
-                           generalTracksFilter
+                           tightMuonsForZ * #generalTracksFilter *
+                           goodTracks *
+                           trackCands *
+                           trackProbes *
+                           dimuons *
+                           dimuonsFilter
                            )
 
 
