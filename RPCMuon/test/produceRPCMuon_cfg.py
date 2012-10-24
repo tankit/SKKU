fc049647441bc5f4f788e7a7402d3b573f55f216
jhgoh <>
2012-10-24 11:06:30 +0000

    on the fly production with reco dataset

--- RPCMuon/test/produceRPCMuon_cfg.py
+++ RPCMuon/test/produceRPCMuon_cfg.py
@@ -1,30 +1,38 @@
 import FWCore.ParameterSet.Config as cms
 
-MC_flag = False
-GLOBAL_TAG = 'GR_R_52_V8::All'
-if MC_flag:
-    GLOBAL_TAG = 'START52_V5::All'
-
 process = cms.Process("TEST")
 
-process.load("Configuration.StandardSequences.RawToDigi_cff")
+#process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.GlobalTag.globaltag = GLOBAL_TAG
+process.GlobalTag.globaltag = 'START53_V9::All'
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
-)   
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:/afs/cern.ch/work/m/mskim/public/rpc/temp/00AD4245-A5B5-E111-A1E8-001EC9D8B54A.root',
-        'file:/afs/cern.ch/work/m/mskim/public/rpc/temp/0215F1C2-BBAE-E111-A01F-485B39800B69.root',
+        #"/store/relval/CMSSW_5_3_4_cand1-START53_V10/RelValProdQCD_Pt_3000_3500/GEN-SIM-RECO/v1/0000/96CBE8F4-78F5-E111-99CB-003048FFD71A.root",
+        #"/store/relval/CMSSW_5_3_4_cand1-START53_V10/RelValProdQCD_Pt_3000_3500/GEN-SIM-RECO/v1/0000/E2C3D691-A4F5-E111-9624-0018F3D0968A.root",
+        "file:///tmp/jhgoh/96CBE8F4-78F5-E111-99CB-003048FFD71A.root",
     ),
     inputCommands = cms.untracked.vstring(
+        "drop *_*_*_RECO",
+
+        "keep *_generalTracks_*_*",
+        "keep *_tevMuons_*_*",
+        "keep *_siPixel*_*_*", "keep *_siStrip*_*_*",
+        "keep *_dt*_*_*", "keep *_csc*_*_*", "keep *_rpc*_*_*",
+        "keep *_*Digi_*_*", "keep *_*Digis_*_*", 
+        "keep *EcalRecHit*_*_*_*", "keep *H*RecHit*_h*reco_*_*",
+        "keep *CaloTower*_*_*_*",
+        "keep *_*CaloJets_*_*",
+        "keep *_offlineBeamSpot_*_*",
+        "keep *_offlinePrimaryVertices_*_*",
     ),
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
 )
 
 process.out = cms.OutputModule("PoolOutputModule",
@@ -35,17 +43,35 @@ process.out = cms.OutputModule("PoolOutputModule",
 from Configuration.EventContent.EventContent_cff import RECOSIMEventContent
 process.out.outputCommands += RECOSIMEventContent.outputCommands
 
+# Good vertex requirement
+process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
+    vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+    minimumNDOF = cms.uint32(4) ,
+    maxAbsZ = cms.double(24),
+    maxd0 = cms.double(2)
+)
+
 process.muidRPCMuLoose = cms.EDProducer("MuonSelectionTypeValueMapProducer",
     inputMuonCollection = cms.InputTag("muons1stStep"),
     selectionType = cms.string('RPCMuLoose'),
 )
 
 process.muonSelectionTypeSequence += process.muidRPCMuLoose
+
+## Disable PFMuon for temporary solution not to crash in 53X
+process.muons.FillPFIsolation = False
+process.muons.FillPFMomentumAndAssociation = False
+
 process.p = cms.Path(
-    process.RawToDigi
-  * process.localreco * process.globalreco
-  + process.egammaHighLevelRecoPrePF + process.particleFlowReco
-  + process.regionalCosmicTracksSeq * process.muoncosmichighlevelreco * process.muonshighlevelreco
+#    process.RawToDigi
+    process.primaryVertexFilter
+  * process.muonrecoComplete
+  * process.muoncosmicreco * process.regionalCosmicTracksSeq
+  * process.muoncosmichighlevelreco #* process.muonshighlevelreco
+  * process.muons
+#    process.localreco * process.globalreco
+#  + process.egammaHighLevelRecoPrePF + process.particleFlowReco
+#  + process.regionalCosmicTracksSeq * process.muoncosmichighlevelreco * process.muonshighlevelreco
 #  * process.particleFlowLinks
 #  * process.jetHighLevelReco * process.tautagging
 #  + process.metrecoPlusHCALNoise + process.btagging * process.recoPFMET + process.PFTau
@@ -57,9 +83,9 @@ process.outPath = cms.EndPath(process.out)
 
 ### User analysis
 
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('hist.root'),
-)
+#process.TFileService = cms.Service("TFileService",
+#    fileName = cms.string('hist.root'),
+#)
 
 process.rpcMuAna = cms.EDAnalyzer("RPCMuonAnalyzer",
     muon = cms.untracked.InputTag("muons"),
@@ -67,12 +93,3 @@ process.rpcMuAna = cms.EDAnalyzer("RPCMuonAnalyzer",
     maxEtaTrk = cms.untracked.double(1.6),
 )
 
-# Good vertex requirement
-process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
-                                           vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-                                           minimumNDOF = cms.uint32(4) ,
-                                           maxAbsZ = cms.double(24),
-                                           maxd0 = cms.double(2)
-                                           )
-
-process.p += process.primaryVertexFilter*process.rpcMuAna
