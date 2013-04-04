e7ae6199c6c91435ed8c3c1c0d9cc7fac8e82e6f
jhgoh <>
2013-04-04 19:38:30 +0000

    default cuts

--- RPCMuon/python/VertexCandProducer_cfi.py
+++ RPCMuon/python/VertexCandProducer_cfi.py
@@ -7,14 +7,13 @@ kshortVertex = cms.EDFilter("VertexCandProducer",
         maxEta = cms.double(2.5),
         chi2 = cms.double(5.),
         nHit = cms.int32(6),
-        ipSignif = cms.double(.5),
+        signif = cms.double(0.5),
         DCA = cms.double(1.),
     ),
     vertex = cms.PSet(
         chi2 = cms.double(7.),
         dxy = cms.double(0.0),
-#        vtxSignif = cms.double(15.0),
-        vtxSignif = cms.double(5.0),
+        signif = cms.double(5.0),
     ),
     pdgId = cms.uint32(310),
     leg1Id = cms.uint32(211),
@@ -45,18 +44,20 @@ phiVertex = kshortVertex.clone(
     rawMassMax = cms.double(1.06),
     massMin = cms.double(1.00),
     massMax = cms.double(1.04),
-    track.ipSignif = cms.double(-5.0),
-    vertex.vtxSignif = cms.double(0.0),
 )
+phiVertex.track.signif = -5
+phiVertex.vertex.signif = -5
+phiVertex.vertex.dxy = -999
 
 jpsiVertex = kshortVertex.clone(
     pdgId = cms.uint32(443),
     leg1Id = cms.uint32(13),
-    leg2id = cms.uint32(13),
+    leg2Id = cms.uint32(13),
     rawMassMin = cms.double(2.90),
     rawMassMax = cms.double(3.30),
     massMin = cms.double(2.95),
     massMax = cms.double(3.25),
-    track.ipSignif = cms.double(-5.0),
-    vertex.vtxSignif = cms.double(0.0),
 )
+jpsiVertex.track.signif = -5
+jpsiVertex.vertex.signif = -5
+jpsiVertex.vertex.dxy = -999
