ba519ba96770658c177614b23fc49e9370ecd7b2
JiwoongSeo <jwseo115@gmail.com>
2013-08-20 17:21:00 +0200

    Move to Symmetric Error

--- RPCGasLeak/test/findLeakyChambers.C
+++ RPCGasLeak/test/findLeakyChambers.C
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-double temperature = 300;
+double temperature = 293;
 
 bool hasValue(const std::vector<int>& arr, int value);
 
@@ -103,6 +103,9 @@ void findLeakyChambers()
   std::vector<int> leakyChambers;
   TF1* fLinear = new TF1("fLinear", func, minTime, maxTime, 2);
   //fLinear->SetParLimits(1, -1, 0);
+  //const double fitMin1 = 900, fitMax1 = 900+1200;
+  //const double fitMin2 = 900, fitMax2 = 900+1200;
+  //const double fitMin3 = 900, fitMax3 = 900+1200;
   const double fitMin1 = 1500, fitMax1 = 1500+600;
   const double fitMin2 = 1200, fitMax2 = 1200+600;
   const double fitMin3 = 900, fitMax3 = 900+600;
@@ -143,9 +146,9 @@ void findLeakyChambers()
     if ( true )
     {
       // Do fitting if it is not leaky chamber - Pressure Drop
-      TFitResultPtr r1 = grp->Fit("fLinear", "QSEM" , "", fitMin1, fitMax1);
-      TFitResultPtr r2 = grp->Fit("fLinear", "+QSEM", "", fitMin2, fitMax2);
-      TFitResultPtr r3 = grp->Fit("fLinear", "+QSEM", "", fitMin3, fitMax3);
+      TFitResultPtr r1 = grp->Fit("fLinear", "QSEMW" , "", fitMin1, fitMax1);
+      TFitResultPtr r2 = grp->Fit("fLinear", "+QSEMW", "", fitMin2, fitMax2);
+      TFitResultPtr r3 = grp->Fit("fLinear", "+QSEMW", "", fitMin3, fitMax3);
 
       TList* functions = grp->GetListOfFunctions();
       TF1* f1 = (TF1*)functions->At(0);
@@ -157,16 +160,19 @@ void findLeakyChambers()
       f3->SetLineColor(kBlue);
 
       const int nGrpPressureDrop = grpPressureDrop1->GetN();
-      const double slope1 = r1->Value(1), errUp1 = r1->UpperError(1), errDn1 = r1->LowerError(1);
-      const double slope2 = r2->Value(1), errUp2 = r2->UpperError(1), errDn2 = r2->LowerError(1);
-      const double slope3 = r3->Value(1), errUp3 = r3->UpperError(1), errDn3 = r3->LowerError(1);
+      //const double slope1 = r1->Value(1), errUp1 = r1->UpperError(1), errDn1 = r1->LowerError(1);
+      //const double slope2 = r2->Value(1), errUp2 = r2->UpperError(1), errDn2 = r2->LowerError(1);
+      //const double slope3 = r3->Value(1), errUp3 = r3->UpperError(1), errDn3 = r3->LowerError(1);
+      const double slope1 = r1->Value(1), errUp1 = f1->GetParError(1), errDn1 = f1->GetParError(1);
+      const double slope2 = r2->Value(1), errUp2 = f1->GetParError(1), errDn2 = f1->GetParError(1);
+      const double slope3 = r3->Value(1), errUp3 = f1->GetParError(1), errDn3 = f1->GetParError(1);
 
       grpPressureDrop1->SetPoint(nGrpPressureDrop, chamberNumber, 600*slope1);
       grpPressureDrop2->SetPoint(nGrpPressureDrop, chamberNumber, 600*slope2);
       grpPressureDrop3->SetPoint(nGrpPressureDrop, chamberNumber, 600*slope3);
-      grpPressureDrop1->SetPointError(nGrpPressureDrop, 0.5, 0.5, -600*errDn1, 600*errUp1);
-      grpPressureDrop2->SetPointError(nGrpPressureDrop, 0.5, 0.5, -600*errDn2, 600*errUp2);
-      grpPressureDrop3->SetPointError(nGrpPressureDrop, 0.5, 0.5, -600*errDn3, 600*errUp3);
+      grpPressureDrop1->SetPointError(nGrpPressureDrop, 0.5, 0.5, 600*errDn1, 600*errUp1);
+      grpPressureDrop2->SetPointError(nGrpPressureDrop, 0.5, 0.5, 600*errDn2, 600*errUp2);
+      grpPressureDrop3->SetPointError(nGrpPressureDrop, 0.5, 0.5, 600*errDn3, 600*errUp3);
 
       hPressureDrop1->Fill(TMath::Min(maxPressureDrop-1e-9,-slope1*600));
       hPressureDrop2->Fill(TMath::Min(maxPressureDrop-1e-9,-slope2*600));
@@ -176,9 +182,9 @@ void findLeakyChambers()
     if ( true )
     {
       // Do fitting if it is not leaky chamber - Leak Rate
-      TFitResultPtr r1 = grpLeak->Fit("fLinear", "QSEM" , "", fitMin1, fitMax1);
-      TFitResultPtr r2 = grpLeak->Fit("fLinear", "+QSEM", "", fitMin2, fitMax2);
-      TFitResultPtr r3 = grpLeak->Fit("fLinear", "+QSEM", "", fitMin3, fitMax3);
+      TFitResultPtr r1 = grpLeak->Fit("fLinear", "QSEMW" , "", fitMin1, fitMax1);
+      TFitResultPtr r2 = grpLeak->Fit("fLinear", "+QSEMW", "", fitMin2, fitMax2);
+      TFitResultPtr r3 = grpLeak->Fit("fLinear", "+QSEMW", "", fitMin3, fitMax3);
 
       TList* functions = grpLeak->GetListOfFunctions();
       TF1* f1 = (TF1*)functions->At(0);
@@ -190,20 +196,23 @@ void findLeakyChambers()
       f3->SetLineColor(kBlue);
 
       const int nGrpLeakRate = grpLeakRate1->GetN();
-      const double slope1 = r1->Value(1), errUp1 = r1->UpperError(1), errDn1 = r1->LowerError(1);
-      const double slope2 = r2->Value(1), errUp2 = r2->UpperError(1), errDn2 = r2->LowerError(1);
-      const double slope3 = r3->Value(1), errUp3 = r3->UpperError(1), errDn3 = r3->LowerError(1);
+      //const double slope1 = r1->Value(1), errUp1 = r1->UpperError(1), errDn1 = r1->LowerError(1);
+      //const double slope2 = r2->Value(1), errUp2 = r2->UpperError(1), errDn2 = r2->LowerError(1);
+      //const double slope3 = r3->Value(1), errUp3 = r3->UpperError(1), errDn3 = r3->LowerError(1);
+      const double slope1 = r1->Value(1), errUp1 = f1->GetParError(1), errDn1 = f1->GetParError(1);
+      const double slope2 = r2->Value(1), errUp2 = f1->GetParError(1), errDn2 = f1->GetParError(1);
+      const double slope3 = r3->Value(1), errUp3 = f1->GetParError(1), errDn3 = f1->GetParError(1);
 
-      grpLeakRate1->SetPoint(nGrpLeakRate, chamberNumber, 600*slope1);
-      grpLeakRate2->SetPoint(nGrpLeakRate, chamberNumber, 600*slope2);
-      grpLeakRate3->SetPoint(nGrpLeakRate, chamberNumber, 600*slope3);
-      grpLeakRate1->SetPointError(nGrpLeakRate, 0.5, 0.5, -600*errDn1, 600*errUp1);
-      grpLeakRate2->SetPointError(nGrpLeakRate, 0.5, 0.5, -600*errDn2, 600*errUp2);
-      grpLeakRate3->SetPointError(nGrpLeakRate, 0.5, 0.5, -600*errDn3, 600*errUp3);
+      grpLeakRate1->SetPoint(nGrpLeakRate, chamberNumber, -600*slope1);
+      grpLeakRate2->SetPoint(nGrpLeakRate, chamberNumber, -600*slope2);
+      grpLeakRate3->SetPoint(nGrpLeakRate, chamberNumber, -600*slope3);
+      grpLeakRate1->SetPointError(nGrpLeakRate, 0.5, 0.5, 600*errUp1, 600*errDn1);
+      grpLeakRate2->SetPointError(nGrpLeakRate, 0.5, 0.5, 600*errUp2, 600*errDn2);
+      grpLeakRate3->SetPointError(nGrpLeakRate, 0.5, 0.5, 600*errUp3, 600*errDn3);
 
-      hLeakRate1->Fill(TMath::Min(maxLeakRate-1e-9,-slope1*600));
-      hLeakRate2->Fill(TMath::Min(maxLeakRate-1e-9,-slope2*600));
-      hLeakRate3->Fill(TMath::Min(maxLeakRate-1e-9,-slope3*600));
+      hLeakRate1->Fill(TMath::Min(maxLeakRate-1e-9,-slope1*600)*(-1.0));
+      hLeakRate2->Fill(TMath::Min(maxLeakRate-1e-9,-slope2*600)*(-1.0));
+      hLeakRate3->Fill(TMath::Min(maxLeakRate-1e-9,-slope3*600)*(-1.0));
     }
 
   }
@@ -222,7 +231,7 @@ void findLeakyChambers()
     TGraph* grp = iter->second;
     grp->Draw("p");
   }
-
+/*
   TCanvas* cGasLeakVsTime = new TCanvas("cGasLeakVsTime", "GasLeak vs Time", 500, 500);
   TH1F* hGasLeakVsTime = new TH1F("hGasLeakVsTime", "GasLeak vs Time;Time (s);GasLeak (mbar)", 100, minTime, maxTime);
   hGasLeakVsTime->SetMinimum(0);
@@ -236,18 +245,19 @@ void findLeakyChambers()
     TGraph* grp = iter->second;
     grp->Draw("p");
   }
+*/
 
   TCanvas* cPressureDropVsChamber = new TCanvas("cPressureDropVsChamber", "Pressure drop vs chamber", 500, 500);
   TH1F* hPressureDropVsChamber = new TH1F("hPressureDropVsChamber", "Pressure drop vs chamber;Chamber index;Pressure drop (mbar/10 min)", (maxChamberNumber-minChamberNumber+1), minChamberNumber, maxChamberNumber+1);
-  hPressureDropVsChamber->SetMinimum(-0.4);//(-1e-3*600);
-  hPressureDropVsChamber->SetMaximum(0.3);//(1e-4);
+  hPressureDropVsChamber->SetMinimum(-0.1);//(-1e-3*600);
+  hPressureDropVsChamber->SetMaximum(0.1);//(1e-4);
   hPressureDropVsChamber->Draw();
   grpPressureDrop1->SetLineColor(kRed);
   grpPressureDrop2->SetLineColor(kGreen);
   grpPressureDrop3->SetLineColor(kBlue);
-  grpPressureDrop1->Draw("P");
-  grpPressureDrop2->Draw("P");
-  grpPressureDrop3->Draw("P");
+  grpPressureDrop1->Draw("PZ");
+  grpPressureDrop2->Draw("PZ");
+  grpPressureDrop3->Draw("PZ");
   TLegend* legPressureDropVsChamber = new TLegend(0.6, 0.17, 0.9, 0.4);
   legPressureDropVsChamber->SetFillStyle(0);
   legPressureDropVsChamber->SetBorderSize(0);
@@ -257,17 +267,17 @@ void findLeakyChambers()
   legPressureDropVsChamber->Draw();
 
   TCanvas* cLeakRateVsChamber = new TCanvas("cLeakRate", "Leak Rate vs Chamber", 500, 500);
-  TH1F* hLeakRateVsChamber = new TH1F("hLeakRateVsChamber", "Leak Rate vs Chamber;Chamber index;Leak Rate (mL/h)", (maxChamberNumber-minChamberNumber+1), minChamberNumber, maxChamberNumber+1);
-  hLeakRateVsChamber->SetMinimum(-1e-3*600);
-  hLeakRateVsChamber->SetMaximum(1e-4);
+  TH1F* hLeakRateVsChamber = new TH1F("hLeakRateVsChamber", "Leak Rate vs Chamber;Chamber index;Leak Rate (mL/10 min)", (maxChamberNumber-minChamberNumber+1), minChamberNumber, maxChamberNumber+1);
+  hLeakRateVsChamber->SetMinimum(-1);
+  hLeakRateVsChamber->SetMaximum(5);
   hLeakRateVsChamber->Draw();
   grpLeakRate1->SetLineColor(kRed);
   grpLeakRate2->SetLineColor(kGreen);
   grpLeakRate3->SetLineColor(kBlue);
-  grpLeakRate1->Draw("P");
-  grpLeakRate2->Draw("P");
-  grpLeakRate3->Draw("P");
-  TLegend* legLeakRateVsChamber = new TLegend(0.6, 0.2, 0.9, 0.4);
+  grpLeakRate1->Draw("PZ");
+  grpLeakRate2->Draw("PZ");
+  grpLeakRate3->Draw("PZ");
+  TLegend* legLeakRateVsChamber = new TLegend(0.63, 0.6, 0.9, 0.9);
   legLeakRateVsChamber->SetFillStyle(0);
   legLeakRateVsChamber->SetBorderSize(0);
   legLeakRateVsChamber->AddEntry(grpLeakRate1, "1500-2100", "l");
