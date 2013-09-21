9515b377b39ecbc8c07db4536fc9c1a4633019fc
Junghwan John Goh <jhgoh@cern.ch>
2013-09-22 00:01:16 +0200

    summary table

--- RPCMuon/test/efficiency/draw.py
+++ RPCMuon/test/efficiency/draw.py
@@ -21,6 +21,7 @@ for varName in varNames:
     leg = TLegend(0.6, 0.25, 0.92, 0.50)
     leg.SetBorderSize(0)
     leg.SetFillStyle(0)
+
     hFrame = None
     for i, muonIdName in enumerate(muonIdNames):
         varDir = modDir.GetDirectory("%s_%s" % (muonIdName, varName))
@@ -30,6 +31,16 @@ for varName in varNames:
             hFrame.GetYaxis().SetTitle("Efficiency")
             hFrame.GetYaxis().SetRangeUser(0.50, 1.00)
             hFrame.Draw()
+
+            tab_header = [varName]
+            for b in range(hFrame.GetNbinsX()):
+                binLo = hFrame.GetXaxis().GetBinLowEdge(b+1)
+                binHi = binLo+hFrame.GetXaxis().GetBinWidth(b+1)
+                tab_header.append("$%.2f-%.2f$" % (binLo, binHi))
+            print "\hline\hline"
+            print " & ".join(tab_header), "\\\\"
+            print "\hline"
+
             objects.append(hFrame)
         hEffic = varDir.Get("hEfficiency")
         if hEffic == None: continue
@@ -38,10 +49,20 @@ for varName in varNames:
         hEffic.SetLineColor(color)
         hEffic.SetMarkerColor(color)
         hEffic.Draw("P")
+
+        tab_content = []
+        for b in range(hEffic.GetN()):
+            y = hEffic.GetY()[b]
+            eyLo = hEffic.GetEYlow()[b]
+            eyHi = hEffic.GetEYhigh()[b]
+            tab_content.append("$%.3f^{+%.3f}_{-%.3f}$" % (y, eyHi, eyLo))
+        print muonIdName, "&",  " & ".join(tab_content), "\\\\"
+
         leg.AddEntry(hEffic, muonIdName, "lp")
 
         objects.append(hEffic)
     leg.Draw()
     c.Print("efficiency_%s.png" % (varName))
     c.Print("efficiency_%s.pdf" % (varName))
+
     objects.extend([c, leg])
